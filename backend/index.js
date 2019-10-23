8d5439eeee4f098aafe2b1c5410ef9efa3bfdc32
francisudeji <francis.udejiofficial@gmail.com>
2019-10-23 10:38:08 +0100

    Initial commit and project setup

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const { ApolloServer, gql } = require('apollo-server')
+
+const books = [
+  {
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling'
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton'
+  }
+]
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+  type Query {
+    books: [Book]
+  }
+`
+
+const resolvers = {
+  Query: {
+    books: () => books
+  }
+}
+
+const server = new ApolloServer({ cors: true, typeDefs, resolvers })
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`)
+})
