6360dd7ca589f1c293733582f3bd407c9367aac4
francisudeji <francis.udejiofficial@gmail.com>
2019-10-25 07:57:42 +0100

    Initialized apollo client

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { ApolloProvider } from '@apollo/react-hooks'
+import ApolloClient from 'apollo-boost'
 import './styles/index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const client = new ApolloClient({
+  uri: 'http://localhost:4000'
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+)
