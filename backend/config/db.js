6549bb1bf5a9051fe482ca75ac171a493734d318
francisudeji <francis.udejiofficial@gmail.com>
2019-10-26 18:41:22 +0100

    Sepatated mongodb connection function from server file to be reused in test

---
+++ backend/config/db.js
@@ -0,0 +1,24 @@
+const keys = require('./keys')
+const mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise
+function connectToDatabase(uri) {
+  return new Promise((resolve, reject) => {
+    mongoose
+      .connect(uri || process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+      })
+
+      .then(() => resolve('Connected'))
+      .catch(err => reject(err))
+  })
+}
+
+function disconnectFromDatabase() {
+  return mongoose.disconnect()
+}
+
+module.exports = { connectToDatabase, disconnectFromDatabase }
